a8f6ff2d35864fe4f6eb7dee74e512ec2c45b607
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2023-04-25 20:09:37 +0300

    fixed interface issue of IPostService

--- EFDataAccessLibrary/Services/PostsService/IPostsService.cs
+++ EFDataAccessLibrary/Services/PostsService/IPostsService.cs
@@ -13,7 +13,7 @@ public interface IPostsService
     public Task<IEnumerable<Post>> GetAllPosts();
     public Task<IEnumerable<Post>> SearchPost(string searchTerm);
     public Task<Post> GetOnePost(int id);
-    public Task<string> CreatePostByAdmin(Post post);
+    public Task<Post> CreatePostByAdmin(Post post);
     public Task<Post> CreatePostByUser(Post post);
     public Task<string> EditPost(PostEditDTO postEditDTO);
     public Task<string> DeletePost(int id);
