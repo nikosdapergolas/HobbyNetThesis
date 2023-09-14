52304fb1e86e716dc0e269ea6dcb4d8f9a5dc360
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2023-09-14 12:13:59 +0300

    moving on to the comments

--- HobbyNetWebsite/Pages/Posts.razor.js
+++ HobbyNetWebsite/Pages/Posts.razor.js
@@ -1,1 +1,12 @@
-﻿
+﻿//document.getElementById("likebutton").addEventListener("click", function (event) {
+//    event.preventDefault(); // Prevents the default behavior of the click event
+//    console.log("Link clicked, but default behavior prevented");
+//});
+//$(function () {
+//    console.log("page is ready");
+
+//    $("#likebutton").click(function () {
+//        event.preventDefault();
+//        console.log("Like button was clicked");
+//    })
+//});
